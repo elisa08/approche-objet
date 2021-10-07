24ddb0397d9ec017bf1842395d2423735430b5f0
Elisa Lambert <elisa.lambertrondouin@gmail.com>
2021-10-07 11:52:40 +0200

    commit

---
+++ src/entites/TestAdressePostale.java
@@ -0,0 +1,21 @@
+package entites;
+
+public class TestAdressePostale {
+ public static void main(String[] args){
+	 
+	 AdressePostale adr1= new AdressePostale();
+	 adr1.numeroRue=1;
+	 adr1.libelleRue= "impasse des Carignans";
+	 adr1.codePostal= 34730;
+	 adr1.ville= "Montpellier";
+	 
+	 AdressePostale adr2= new AdressePostale();
+	 adr2.numeroRue=1;
+	 adr2.libelleRue= "impasse des Carignans";
+	 adr2.codePostal= 34730;
+	 adr2.ville= "Montpellier";
+	 
+	 
+	 
+ }
+}
